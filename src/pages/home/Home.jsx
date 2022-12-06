09b672caa84d3130a33e6bfad42030fbed3246a9
Mahesh Biswas <maheshbiswas26@gmail.com>
2022-12-06 15:24:49 +0600

    table

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,6 +3,8 @@ import Chart from "../../components/chart/Chart";
 import Featured from "../../components/featured/Featured";
 import Navbar from "../../components/navbar/Navbar";
 import Sidebar from "../../components/sidebar/Sidebar";
+import List from "../../components/table/Table";
+
 import Wiget from "../../components/wiget/Wiget";
 import "./home.scss";
 const Home = () => {
@@ -20,6 +22,10 @@ const Home = () => {
         <div className="charts">
           <Featured />
           <Chart title="Last 6 Months (Revenue)" aspect={2 / 1} />
+        </div>
+        <div className="listContainer">
+          <div className="listTitle">Latest Transactions</div>
+          <List />
         </div>
       </div>
     </div>
